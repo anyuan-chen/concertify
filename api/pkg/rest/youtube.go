533f652a71e7cb513c90a57c758e26fcc00cda9d
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-08-14 12:57:03 -0400

    youtube callback and login

---
+++ api/pkg/rest/youtube.go
@@ -0,0 +1,62 @@
+package rest
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var GoogleConfig = &oauth2.Config{
+	ClientID:     os.Getenv("GOOGLE_ID"),
+	ClientSecret: os.Getenv("GOOGLE_SECRET"),
+	Endpoint:     google.Endpoint,
+	RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+	Scopes:       []string{""},
+}
+
+func (api *ConcertifyAPI) YoutubeLogin(w http.ResponseWriter, r *http.Request) {
+	expiration := time.Now().Add(20 * time.Minute)
+	state := make(map[string]interface{})
+	randomizedState := make([]byte, 16)
+	rand.Read(randomizedState)
+	state["random"] = randomizedState
+	json, err := json.Marshal(state)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	encoded_json := base64.URLEncoding.EncodeToString(json)
+	url := GoogleConfig.AuthCodeURL(encoded_json)
+	cookie := http.Cookie{Name: "oauthstate", Value: encoded_json, Expires: expiration}
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func (api *ConcertifyAPI) YoutubeCallback(w http.ResponseWriter, r *http.Request) {
+	state, err := r.Cookie("oauthstate")
+	if err != nil || r.FormValue("state") != state.Value {
+		http.Error(w, "Bad OAuth State", http.StatusInternalServerError)
+	}
+	token, err := GoogleConfig.Exchange(r.Context(), state.Value)
+	if err != nil {
+		http.Error(w, "Failed to Retrieve Token", http.StatusInternalServerError)
+		return
+	}
+	session_id, err := r.Cookie("session_id")
+	if err != nil {
+		http.Error(w, "No Session Cookie", http.StatusInternalServerError)
+		return
+	}
+	err = api.Session_Manager.SetYoutubeSession(session_id.Value, token)
+	if err != nil {
+		http.Error(w, "Failed to Set Session", http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, os.Getenv("FRONTEND_URL"), http.StatusPermanentRedirect)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/anyuan-chen/concertify
 
 go 1.18
+
+require golang.org/x/oauth2 v0.0.0-20220808172628-8227340efae7
+
+require cloud.google.com/go/compute v1.8.0 // indirect
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/zmb3/spotify/v2 v2.2.1
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
