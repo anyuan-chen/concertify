008eba062af482ab5bad0acf1cff749d5cff4137
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-08-29 17:36:49 -0400

    swr changes

--- web/components/selectplaylist.jsx
+++ web/components/selectplaylist.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const SelectPlaylist = ({ src, title, author }) => {
   return (
-    <div className="flex flex-col bg-gray-100 gap-y-8 py-4">
+    <div className="flex flex-col bg-gray-100 gap-y-4 p-8">
       <img src={src} style={{ width: "300px", height: "300px" }}></img>
-      <h2 className="text-3xl">{title}</h2>
-      <h3 className="text-xl text-gray-600">By {author}</h3>
+      <h2 className="text-3xl pt-4 self-start">{title}</h2>
+      <h3 className="text-2xl text-gray-600 self-start">By {author}</h3>
     </div>
   );
 };
--- web/pages/select.jsx
+++ web/pages/select.jsx
@@ -1,28 +1,58 @@
 import Head from "next/head";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import useSWR from "swr";
 import SelectPlaylist from "../components/selectplaylist";
 
-const Select = ({ playlists }) => {
+const Select = () => {
+  const fetcher = (url, args) => fetch(url, args).then((res) => res.json());
+  const { data, error } = useSWR(
+    [
+      process.env.NEXT_PUBLIC_BACKEND_URL + "/api/playlists",
+      {
+        credentials: "include",
+        mode: "cors",
+      },
+    ],
+    fetcher
+  );
+  if (error) return "An error has occurred.";
+  if (!data) return "Loading...";
   return (
     <div className="flex flex-col gap-y-8">
       <h1 className="font-medium text-6xl">Select your playlist</h1>
       <div className="flex flex-wrap gap-8">
-        {playlists.map((playlist) => (
-          <SelectPlaylist></SelectPlaylist>
+        {data?.items?.map((playlist) => (
+          <button>
+            <SelectPlaylist
+              src={playlist?.images[0]?.url}
+              title={playlist?.name}
+              author={playlist?.owner?.display_name}
+            ></SelectPlaylist>
+          </button>
         ))}
       </div>
     </div>
   );
 };
 
-export async function getServerSideProps(context) {
-  const sessionCookie = context.req.cookies["session_id"];
-  console.log(sessionCookie);
-  return {
-    props: {
-      playlists: [],
-    },
-  };
-}
+// export async function getServerSideProps(context) {
+//   const sessionCookie = context.req.cookies["session_id"];
+// const req = await fetch(
+//   `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/playlists`,
+//   {
+//     credentials: "include",
+//     mode: "cors",
+//     method: "GET",
+//   }
+// );
+//   console.log(req);
+//   //const data = await req.json();
+//   //console.log(data);
+//   return {
+//     props: {
+//       playlists: [],
+//     },
+//   };
+// }
 export default Select;
